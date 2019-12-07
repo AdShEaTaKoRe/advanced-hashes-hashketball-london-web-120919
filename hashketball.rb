bf1f4a08ab6cfc20c5e8d69f6767a85c565f9e77
Tommy Berman <tomy.berman1@gmail.com>
2019-12-07 01:52:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,7 +27,7 @@ def game_hash()
   }
 end
 
-def num_points_scored()
+def num_points_scored(player_name)
   data = game_hash()
   pp data
   
@@ -36,7 +36,7 @@ def num_points_scored()
       memo[player[:player_name]] = player[:poins]
     }
     memo  
-  }
+  }[player_name]
 end
 
 
