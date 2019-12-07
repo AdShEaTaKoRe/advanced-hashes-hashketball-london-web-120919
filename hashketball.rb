3e0af1fad12bec4392d8dc113b1640f282bf6b00
Tommy Berman <tomy.berman1@gmail.com>
2019-12-07 01:57:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,8 +30,8 @@ end
 def num_points_scored(player_name)
   data = game_hash()
   
-  data.each { |team_data|
-    found_player = team_data[:players].find { |player| player[:player_name] == player_name }
+  data.each { |team|
+    found_player = team[:players].find { |player| player[:player_name] == player_name }
     
     if found_player
       return player[:points]
